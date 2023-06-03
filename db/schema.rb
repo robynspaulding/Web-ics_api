ebf55505105ec9b88f143a4c5c5504e41c150071
RobynSpaulding <robynm27@gmail.com>
2023-06-02 20:32:43 -0700

    creates comments join table to connect users and commics and migrates

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_03_030732) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_03_033149) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comics", force: :cascade do |t|
     t.string "title"
     t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "comic_id"
+    t.string "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
