ac2e6a41baecb02055b4a2eb62cf4477123b8e20
RobynSpaulding <robynm27@gmail.com>
2023-06-02 20:50:10 -0700

    adds all crud methods and authentications to comments controller, tests actions in reqeusts file, all successfull

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,39 @@
 class CommentsController < ApplicationController
+
+  before_action :authenticate_user, except: [:index, :show]
+  
+  def index
+    comments = Comment.all
+    render json: comments.as_json
+  end
+
+  def create
+    comment = Comment.create(
+      user_id: params[:user_id],
+      comic_id: params[:comic_id],
+      comment: params[:comment],
+    )
+    render json: comment.as_json
+  end
+
+  def show
+    comment = Comment.find_by(id: params[:id])
+    render json: comment.as_json
+  end
+
+  def update
+    comment = Comment.find_by(id: params[:id])
+    comment.user_id = params[:user_id] || comment.user_id
+    comment.comic_id = params[:comic_id] || comment.comic_id
+    comment.comment = params[:comment] || comment.comment
+    comment.save
+    render json: comment.as_json
+  end
+
+  def destroy
+    comment = Comment.find_by(id: params[:id])
+    comment.delete
+    render json: { message: "Comment successfullt deleted" }
+  end
+
 end
