9c352544a531b2f3e419a4f2cb64feb6096e8267
RobynSpaulding <robynm27@gmail.com>
2023-06-02 20:41:48 -0700

    adds more data to seeds file, checks relationships in rails console, working

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,14 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-Comic.create(title: "did you guys see Sandman yet", image: "sandman.jpg")
-Comic.create(title: "I love Star soooo much", image: "stars.png")
-Comic.create(title: "It's corn!", image: "cornman.png")
+Comic.create(title: "Sandman vs. Sandwitch", image: "sandman.jpg")
+Comic.create(title: "I love stars soooo much", image: "stars.png")
+Comic.create(title: "It's corn!", image: "corny.png")
+
+User.create(name: "Sam Jones", email: "sam@test.com", password: "password", password_confirmation: "password")
+User.create(name: "Jane Doe", email: "jane@test.com", password: "password", password_confirmation: "password")
+
+
+Comment.create(user_id: 1, comic_id: 1, comment: "I love this!")
+Comment.create(user_id: 1, comic_id: 3, comment: "Corny as a Southern summer!")
+Comment.create(user_id: 2, comic_id: 2, comment: "I don't like this, needs more ... stars? ")
