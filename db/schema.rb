228986de726b9eaea3a51f420340586e7ce25266
RobynSpaulding <robynm27@gmail.com>
2023-06-02 20:08:12 -0700

    adds bcrypt gem to gemfile, creates user model table, migrates user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_02_033210) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_03_030732) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comics", force: :cascade do |t|
     t.string "title"
     t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
